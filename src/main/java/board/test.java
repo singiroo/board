2b5c7cd75eebe98602d96f9a2a37a0552d4393a1
202005_kyh <linkan.kim@gmail.com>
2020-10-24 16:42:04 +0900

    20201024 16:40 과제용 git repository 경 first commit  축!!

---
+++ src/main/java/board/test.java
@@ -0,0 +1,14 @@
+package board;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class test {
+	private static final Logger logger = LoggerFactory.getLogger(test.class);
+	/***Given***/
+		//테스트 입니다.
+	/***When***/
+		//git 전송용
+	/***Then***/
+		//git 전송되어라!
+}
